6b33a19b1fa3592cdab7b65994affa6e5f51e47c
Zachary Scott <zachary@zacharyscott.net>
2013-07-11 12:49:04 -0400

    import

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :title, :author, :body, :created_at, :updated_at
---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+require 'securerandom'
+
+15.times do
+  Post.create(
+    :title => "My Post #{SecureRandom.hex(2)}",
+    :author => SecureRandom.hex(6),
+    :body => SecureRandom.hex(32)
+  )
+end
